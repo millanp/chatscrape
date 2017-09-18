e25a6ce5795fdd575c61e8dc51b8f8c048c840d5
millanp <millan.philipose@gmail.com>
2017-09-17 18:28:07 -0700

    working version

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -6,8 +6,83 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+
+var watchTabId, chatTabId, vidId;
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.action == "setVidIdAndSubCount") {
+            chrome.tabs.getSelected(null, function(tab) {
+                watchTabId = tab.id;
+                vidId = request.vidId;
+                subCount = request.subCount;
+                dlSubCount(subCount);
+                alert(watchTabId);
+            });
+        }
+        if (request.action == "setChatTabId") {
+            chrome.tabs.getSelected(null, function(tab) {
+                chatTabId = tab.id;
+                alert(chatTabId);
+            });
+        }
+        // var downloadUrl = "data:,";
+        // downloadUrl += encodeURIComponent(request.urlList);
+        // chrome.downloads.download({
+        //     url: downloadUrl,
+        //     filename: request.name
+        // })
+    }
+);
+
+function dlSubCount(subCount) {
+    chrome.downloads.download({
+        url: "data:," + subCount,
+        filename: vidId + "-subs-" + (new Date()).getTime()
+    });
+}
+
+var clockOn = true;
+var writeIntervalMillis = 10 * 1000;
+var downloadClock;
+
+function startClock() {
+    downloadClock = setInterval(function() {
+        var time = (new Date()).getTime();
+        chrome.tabs.sendMessage(chatTabId, { action: "getData" }, (function(currentTime) {
+            return function(response) {
+                var chats = response.chatsObject;
+                var fileName = vidId + "-chat-" + currentTime;
+                var downloadUrl = "data:," + encodeURIComponent(JSON.stringify(chats));
+                chrome.downloads.download({
+                    url: downloadUrl,
+                    filename: fileName
+                });
+            }
+        })(time));
+        chrome.tabs.sendMessage(watchTabId, { action: "getData" }, (function(currentTime) {
+            return function(response) {
+                var views = response.viewsObject;
+                var fileName = vidId + "-views-" + currentTime;
+                var downloadUrl = "data:," + encodeURIComponent(JSON.stringify(views));
+                chrome.downloads.download({
+                    url: downloadUrl,
+                    filename: fileName
+                });
+            }
+        })(time));
+
+    }, writeIntervalMillis)
+}
+
+startClock();
+
+chrome.browserAction.onClicked.addListener(function() {
+    if (clockOn) {
+        clearInterval(downloadClock);
+        chrome.browserAction.setIcon({ path: '/icons/redcircle.png' });
+    } else {
+        startClock();
+        chrome.browserAction.setIcon({ path: '/icons/greencircle.png' });
+    }
+    clockOn = !clockOn;
+});
